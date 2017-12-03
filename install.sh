07a7938d15e40fdccdffe7b73f0d4fcdf9dec1be
hrsma2i <hrs.ma2i@gmail.com>
2017-12-04 00:52:55 +0900

    Add ln .vimrc

--- install.sh
+++ install.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
-cd ~/.dotfiles
-
-for f in .??*
-do
-    [[ "$f" == ".git" ]] && continue
-    [[ "$f" == ".DS_Store" ]] && continue
-
-    echo "$f"
-done
+ln -fns ~/.dotfiles/.vimrc ~/.vimrc
